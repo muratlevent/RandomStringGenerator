11fec8b4e9df8941013643318611af5e0fe9dfce
Murat Levent <levent.murat@icloud.com>
2024-03-10 23:33:38 +0300

    constants update

--- src/main/java/com/example/StringGenerator/RandomStringGenerator.java
+++ src/main/java/com/example/StringGenerator/RandomStringGenerator.java
@@ -6,17 +6,21 @@ import java.io.IOException;
 import java.util.Random;
 
 public class RandomStringGenerator {
-    public static void main(String[] args) {
-        int totalLines = 10;
-        String fileName = "randomStrings.txt";
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
-            for (int i = 0; i < totalLines; i++) {
-                String randomString = generateRandomString(8, 20);
+    private static final String FILE_NAME = "randomStrings.txt";
+    private static final int TOTAL_LINES = 10;
+    private static final int MIN_STRING_LENGTH = 8;
+    private static final int MAX_STRING_LENGTH = 20;
+
+    public static void main(String[] args) {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
+            for (int i = 0; i < TOTAL_LINES; i++) {
+                String randomString = generateRandomString(MIN_STRING_LENGTH, MAX_STRING_LENGTH);
                 writer.write(randomString);
                 writer.newLine();
             }
-            System.out.println("File created: " + fileName);
+            System.out.println("File created: " + FILE_NAME);
         } catch (IOException e) {
             System.err.println("Error occurred while writing to file: " + e.getMessage());
         }
